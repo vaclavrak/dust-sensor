e37da0a4c591d2198d5980dc772d23f5c937228e
Rak Václav <rak@avast.com>
2015-11-01 22:45:37 +0100

    initial commit

---
+++ dust_sensor/DustSensor.py
@@ -0,0 +1,19 @@
+"""
+
+ Read data from dust sensor and store it to statsite
+
+ by Vaclav Rak  <me@vena.cz>
+
+"""
+
+class DustSensor(object):
+    _serial_port = ""
+    _serial_speed = 9600
+    _vetilation_gpio = None
+
+    def __init__(self, ventilation_gpio, serial_port='/dev/ttyAMA0', serial_speed=9600):
+        self._serial_port = serial_port
+        self._serial_speed = serial_speed
+        self._vetilation_gpio = ventilation_gpio
+
+    
