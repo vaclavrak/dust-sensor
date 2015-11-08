7c8da8da9d8cb15ec3c60ce5472319e74255614e
Rak Václav <rak@avast.com>
2015-11-08 20:41:40 +0100

    small updates

--- dust_sensor/DustSensor.py
+++ dust_sensor/DustSensor.py
@@ -6,13 +6,14 @@
 
 """
 
+
 class DustSensor(object):
     _serial_port = ""
     _serial_speed = 9600
-    _vetilation_gpio = None
+    _ventilation_gpio = None
 
     def __init__(self, ventilation_gpio, serial_port='/dev/ttyAMA0', serial_speed=9600):
         self._serial_port = serial_port
         self._serial_speed = serial_speed
-        self._vetilation_gpio = ventilation_gpio
+        self._ventilation_gpio = ventilation_gpio
 
